0670d12c5fd9424539f8f09f68a9c9d85a13d9c0
Govind sah <govindx2026@gmail.com>
2026-04-08 16:01:07 +0545

    change script & add tag

--- Untiy game project/Assets/Scripts/Player/PlayerController.cs
+++ Untiy game project/Assets/Scripts/Player/PlayerController.cs
@@ -12,13 +12,22 @@ public class PlayerController : MonoBehaviour
     float rotationX;
     float rotationY;
 
+    [Header("Player Movement")]
+    public float speed = 1;
+    float moveX;
+    float moveY;
+    float moveZ;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         t = this.transform;
         Cursor.lockState = CursorLockMode.Locked;
     }
-
+    private void FixedUpdate()
+    {
+        Move();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -34,5 +43,14 @@ public class PlayerController : MonoBehaviour
         rotationY += Input.GetAxis("Mouse Y") * sensitivity;
         rotationY = Mathf.Clamp(rotationY, rotationMin, rotationMax);
         t.localRotation = Quaternion.Euler(-rotationY, rotationX, 0);
+    }
+    void Move()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        moveZ = Input.GetAxis("Forward");
+
+        t.Translate(new Vector3(moveX, 0, moveZ) * speed * Time.deltaTime);
+        t.Translate(new Vector3(0, moveY, 0) * speed * Time.deltaTime, Space.World);
     }
 }
