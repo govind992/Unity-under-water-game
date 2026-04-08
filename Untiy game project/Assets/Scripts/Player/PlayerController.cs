836005af5b87cbe772462bf50055ab9898bbafa4
Govind sah <govindx2026@gmail.com>
2026-04-08 14:39:40 +0545

    add sky

--- Untiy game project/Assets/Scripts/Player/PlayerController.cs
+++ Untiy game project/Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   Transform t;
+[Header("Player Rotation")]
+public float sensitivity = 1;
+
+
+float rotationX;
+float rotationY;
+
+
     void Start()
     {
-        
+       t =  this.transform;
+       cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        LookAround();
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    void LookAround()
+    {
+        rotationX += Input.GetAxis("Mouse X") * sensitivity;
+        rotationY += Input.GetAxis("Mouse Y") * sensitivity;
+
         
+
+        t.localRotation = Quaternion.Euler(-rotationY, rotationX, 0);
     }
 }
