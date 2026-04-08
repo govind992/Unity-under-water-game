a830a069dfa60fef769cd5b500380d5bec9adffd
Govind sah <govindx2026@gmail.com>
2026-04-08 15:44:03 +0545

    add player controller

--- Untiy game project/Assets/Scripts/Player/PlayerController.cs
+++ Untiy game project/Assets/Scripts/Player/PlayerController.cs
@@ -1,41 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-   Transform t;
-[Header("Player Rotation")]
-public float sensitivity = 1;
+    Transform t;
+    [Header("Player Rotation")]
+    public float sensitivity = 1;
 
+    public float rotationMin;
+    public float rotationMax;
+    float rotationX;
+    float rotationY;
 
-float rotationX;
-float rotationY;
-
-
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       t =  this.transform;
-       cursor.lockState = CursorLockMode.Locked;
+        t = this.transform;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        LookAround();
-        if(Input.GetKeyDown(KeyCode.Escape))
+       LookAround(); 
+       if(Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
         }
     }
-
     void LookAround()
     {
         rotationX += Input.GetAxis("Mouse X") * sensitivity;
         rotationY += Input.GetAxis("Mouse Y") * sensitivity;
-
-        
-
+        rotationY = Mathf.Clamp(rotationY, rotationMin, rotationMax);
         t.localRotation = Quaternion.Euler(-rotationY, rotationX, 0);
     }
 }
